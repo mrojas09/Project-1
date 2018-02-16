80b9a01ee2b47d82eff82b56c95be2a8483fe904
Kerin Grewal <keringrewal@dhcp-wifi-8021x-168-122-216-76.bu.edu>
2018-02-16 00:33:12 -0500

    Added writeup and monthly summaries to shiny

--- Shiny/app.R
+++ Shiny/app.R
@@ -12,8 +12,9 @@ library(datasets)
 
 #import data from CSVs
 data2010 <- read.csv('BP Apprehensions 2010.csv')
-data2017 <- read.csv('PB Apprehensions 2017.csv')
-
+data2017 <- read.csv('BP Apprehensions 2017.csv')
+ts <- read.csv('BP monthly summaries.csv')
+ts1 <- ts[2:18, 2:13]
 
 # Define UI for application
 
@@ -21,7 +22,7 @@ data2017 <- read.csv('PB Apprehensions 2017.csv')
 ui <- fluidPage(
   
   #title
-  titlePanel("Boarder Patrol Apprehensions by Sector in 2010 and 2017"),
+  titlePanel("US-Mexico Boarder Patrol Apprehensions"),
   
   #sidebar where you can select the sector
   sidebarLayout(
@@ -29,13 +30,13 @@ ui <- fluidPage(
     sidebarPanel(
       
       selectInput('comp', 'Choose comparison', 
-                  choices = c('Big Bend' = '1', 'Del Rio' = '2', 'El Centro' = '3', 'El Paso' = '4', 'Laredo' = '5', 'Rio Grande Valley' = '6', 'San Diego' = '7', 'Tuscon' = '8', 'Yuma' = '9'),
+                  choices = c('Monthly Summaries'= '10', 'Big Bend' = '1', 'Del Rio' = '2', 'El Centro' = '3', 'El Paso' = '4', 'Laredo' = '5', 'Rio Grande Valley' = '6', 'San Diego' = '7', 'Tuscon' = '8', 'Yuma' = '9'),
                   #choices = data2010$Sector,
                   selected = 'Big Bend'),
       
-      imageOutput('writeup.pdf', width = 500, height = 500)
+      includeText('writeup.txt')
     ),
-    
+
     #Location of barplot
     mainPanel(
       tabsetPanel(
@@ -53,22 +54,28 @@ server <- function(input, output) {
   
   #Reactive which processes data based on selected Sector
   selectedData <- reactive({
+    if (as.numeric(input$comp) != 10){
+      selected = as.numeric(input$comp)
     
-    selected = as.numeric(input$comp)
+      #take the selected row from each dataset 
+      d1 = as.data.frame(data2010[c(selected),])
+      d2 = as.data.frame(data2017[c(selected),])
+      
+      #Bind the data from each dataset
+      mydata <- rbind(d1, d2)
     
-    #take the selected row from each dataset 
-    d1 = as.data.frame(data2010[c(selected),])
-    d2 = as.data.frame(data2017[c(selected),])
     
-    #Bind the data from each dataset
-    mydata <- rbind(d1, d2)
+      #Take out the row which lists the sector
+      mydata$Sector <- NULL
+      #Specify which row is which year
+      rownames(mydata) <- c("2010", "2017")
+      
+      return(mydata)
+    }
+    else{
+      return(ts)
+    }
     
-    #Take out the row which lists the sector
-    mydata$Sector <- NULL
-    #Specify which row is which year
-    rownames(mydata) <- c("2010", "2017")
-    
-    return(mydata)
     
   })
 
@@ -77,17 +84,26 @@ server <- function(input, output) {
     #Take reactive data
     mydata <- selectedData()
     
+    if (as.numeric(input$comp) != 10){
     #Plot the data
-    barplot(as.matrix(mydata), 
-            beside = TRUE,
-            xlab = "Months",
-            ylab = "Apprehensions",
-            names.arg = c("October", "November", "December", "January", "February", "March", "April", "May", "June", "July", "August", "September"),
-            col = c("yellow", "blue"), 
-            bty="n" )
-    
-        legend("topright", c("2010","2017"), pch=15,  col=c("yellow","blue"),  bty="n")
-     
+      barplot(as.matrix(mydata), 
+              beside = TRUE,
+              xlab = "Months",
+              ylab = "Apprehensions",
+              names.arg = c("October", "November", "December", "January", "February", "March", "April", "May", "June", "July", "August", "September"),
+              col = c("yellow", "blue"), 
+              bty="n" )
+      
+          legend("topright", c("2010","2017"), pch=15,  col=c("yellow","blue"),  bty="n")
+    }
+    else {
+      
+      ts2 <- as.vector(t(ts1))
+      
+      ts3 <- ts(ts2, start = c(2000, 1), frequency=12)
+      
+      ts.plot(ts3, gpars=list(xlab="year", ylab="Apprehensions", col= 'green', lty=c(1:3)))
+    }
    })
   
   output$table <- renderTable(selectedData())
