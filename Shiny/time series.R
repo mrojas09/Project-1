c4c13fc9802585c8c10382ed44432f8da7ee0b48
Kerin Grewal <keringrewal@dhcp-wifi-8021x-155-41-25-52.bu.edu>
2018-02-16 15:21:13 -0500

    Trying to label time series

--- Shiny/time series.R
+++ Shiny/time series.R
@@ -2,6 +2,10 @@
 # copy the data from the screen w/o year and month
 
 ts1 <- read.csv('BP monthly summaries.csv')
+show(ts1)
+
+year <- as.vector(ts1['year'])
+
 ts1 <- ts1[2:18, 2:13]
 
 ts2 <- as.vector(t(ts1))
@@ -10,3 +14,4 @@ ts3 <- ts(ts2, start = c(2000, 1), frequency=12)
 
 ts.plot(ts3, gpars=list(xlab="year", ylab="Apprehensions", col= 'green', lty=c(1:3)))
 
+text(ts3, labels = as.character(year), cex = 0.7)
