38b6aa4c2e4378e43da01c67ed5cbf05b086b6d4
Kerin Grewal <keringrewal@dhcp-wifi-8021x-168-122-216-76.bu.edu>
2018-02-15 23:30:33 -0500

    Changed monthly summaries data and timeseries.R

--- Shiny/time series.R
+++ Shiny/time series.R
@@ -1,15 +1,9 @@
 # use monthly summary
 # copy the data from the screen w/o year and month
 
-myvect = read.csv("PB monthly summaries.csv")
+myvect <- read.csv('')
 
-ts <- ts(myvect, start=c(2017, 1), end = c(2000, 13), frequency = 12)
+ts1 <- ts(myvect, start=c(2017, 1), frequency = 12)
 
-ts1 <- read.table("clipboard", sep = "\t")
-ts1
+plot.ts(ts1)
 
-ts2 <- as.vector(t(ts1))
-
-ts3 <- ts(ts2, start = c(2000,10), frequency=12)
-
-ts.plot(ts3, gpars=list(xlab="year", ylab="Apprehensions", lty=c(1:3)))
